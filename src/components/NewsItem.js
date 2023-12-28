ee80b742596aa768c0bf038efde9ef6c4848ced6
Kinjal Jagani <kinjalakabari@gmail.com>
2023-12-28 12:36:28 +0530

    add all files

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div>
         <div className="card" style={{width: "18rem"}}>
-          <img src={imgUrl} className="card-img-top" alt="..." />
+          <img src={!imgUrl?"https://static.tnn.in/thumb/msid-106308149,thumbsize-173122,width-1280,height-720,resizemode-75/106308149.jpg": imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}... </h5>
+            <h5 className="card-title">{title} </h5>
             <p className="card-text">
-              {description}...
+              {description}
             </p>
             <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">
               Read More...
